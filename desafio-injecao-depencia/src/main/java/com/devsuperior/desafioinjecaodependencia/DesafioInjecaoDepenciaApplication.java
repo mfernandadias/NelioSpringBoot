97de55cbd704ad8c7946f34423a1d8244fe0c88c
Fernanda Dias <mariadosantodias@gmail.com>
2025-12-04 17:29:38 -0300

    codigo 02

---
+++ desafio-injecao-depencia/src/main/java/com/devsuperior/desafioinjecaodependencia/DesafioInjecaoDepenciaApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafioinjecaodependencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioInjecaoDepenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioInjecaoDepenciaApplication.class, args);
+	}
+
+}
---
+++ dscommerce/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_category(name) VALUES ('Livros');
+INSERT INTO TB_CATEGORY(name) VALUES ('Eletrônicos');
+
+INSERT INTO tb_product(name, description, price, img_url) VALUES ('Senhor dos Anéis', 300.0, 'jhtkn.livros.com.br' )
