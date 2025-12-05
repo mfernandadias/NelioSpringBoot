b85dbbbcbf1f71ea5a70ad469d58a65752efe88a
Fernanda Dias <mariadosantodias@gmail.com>
2025-12-04 21:46:56 -0300

    User java

---
+++ dscommerce/src/main/java/com/devSuperior/dscommerce/entities/User.java
@@ -0,0 +1,112 @@
+package com.devSuperior.dscommerce.entities;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.Columns;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_user")
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String phone;
+
+    //@Column(columnDefinition = "TEXT")
+    private String description;
+    private LocalDate birthDate;
+    private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
+
+    public User(){}
+
+    public User(Long id, String password, LocalDate birthDate, String phone, String email, String name) {
+        this.id = id;
+        this.password = password;
+        this.birthDate = birthDate;
+        this.phone = phone;
+        this.email = email;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    /*public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }*/
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+
+}
