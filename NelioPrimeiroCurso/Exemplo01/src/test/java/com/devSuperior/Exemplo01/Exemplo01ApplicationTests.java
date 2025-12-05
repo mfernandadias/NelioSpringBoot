73ab7858976cbde290e40f382d98da6d078ef62b
Fernanda Dias <mariadosantodias@gmail.com>
2025-12-04 21:50:37 -0300

    application

---
+++ NelioPrimeiroCurso/Exemplo01/src/test/java/com/devSuperior/Exemplo01/Exemplo01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devSuperior.Exemplo01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exemplo01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
