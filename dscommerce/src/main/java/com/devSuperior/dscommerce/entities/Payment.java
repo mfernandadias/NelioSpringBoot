fd748e06b3cdd839dd0a808de888e9e9feea56ce
Fernanda Dias <mariadosantodias@gmail.com>
2025-12-04 21:46:17 -0300

    payment java

---
+++ dscommerce/src/main/java/com/devSuperior/dscommerce/entities/Payment.java
@@ -0,0 +1,56 @@
+package com.devSuperior.dscommerce.entities;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_payment")
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant moment;
+
+
+    @OneToOne
+    @MapsId
+    private Order order;
+
+    public Payment(){
+
+    }
+
+    public Payment(Long id, Order order, Instant moment) {
+        this.id = id;
+        this.order = order;
+        this.moment = moment;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+}
