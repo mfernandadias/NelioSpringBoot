f0469aed7e208b3f6d6e6bf1b1c76e91b1dd91ca
Fernanda Dias <mariadosantodias@gmail.com>
2025-12-04 21:41:41 -0300

    desafio injecao

--- desafio-injecao-depencia/src/test/java/com/devsuperior/desafioinjecaodependencia/DesafioInjecaoDependenciaApplication.java
+++ desafio-injecao-depencia/src/test/java/com/devsuperior/desafioinjecaodependencia/DesafioInjecaoDependenciaApplication.java
@@ -26,10 +26,21 @@ public class DesafioInjecaoDependenciaApplication implements CommandLineRunner  
     @Override
     public void run(String... args) throws Exception {
 
+
+        //Order 1
         Order order = new Order(1034, 20.0, 150.0);
         System.out.println("----");
         System.out.println("Pedido código " + order.getCode());
         System.out.println("Valor total: R$ " + orderService.total(order));
+
+
+        //Order 2
+        Order order1 = new Order(1234, 20.0, 200.0);
+        System.out.println("------");
+        System.out.println("Pedido código " + order1.getCode());
+        System.out.println("Pedido código " + orderService.total(order1));
+
+
     }
 
 
