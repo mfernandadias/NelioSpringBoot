7bb8ff0cc22540e169703243e40433ab67c8369b
Fernanda Dias <mariadosantodias@gmail.com>
2026-02-20 23:57:28 -0300

    atividade repositori

---
+++ challenge02/src/main/java/com/javaProfessional/challenge02/repository/AtividadeRepository.java
@@ -0,0 +1,9 @@
+package com.javaProfessional.challenge02.repository;
+
+import com.javaProfessional.challenge02.entities.Atividade;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AtividadeRepository extends JpaRepository<Atividade, Long> {
+
+
+}
