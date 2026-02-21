3a2a996049c922cd1d6e4a5d4fded7283e98014c
Fernanda Dias <mariadosantodias@gmail.com>
2026-02-20 23:57:40 -0300

    bloco repository

---
+++ challenge02/src/main/java/com/javaProfessional/challenge02/repository/BlocoRepository.java
@@ -0,0 +1,8 @@
+package com.javaProfessional.challenge02.repository;
+
+import com.javaProfessional.challenge02.entities.Bloco;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BlocoRepository extends JpaRepository<Bloco, Long> {
+
+}
