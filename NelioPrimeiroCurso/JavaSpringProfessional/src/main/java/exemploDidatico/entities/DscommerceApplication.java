3d6e18fa9d8643f624e0e9c2f831d0885c8bd845
Fernanda Dias <mariadosantodias@gmail.com>
2026-02-21 00:00:21 -0300

    dscommerce

---
+++ NelioPrimeiroCurso/JavaSpringProfessional/src/main/java/exemploDidatico/entities/DscommerceApplication.java
@@ -0,0 +1,14 @@
+package exemploDidatico.entities;
+
+public class DscommerceApplication {
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String birtDate;
+    private String password;
+
+
+
+
+}
