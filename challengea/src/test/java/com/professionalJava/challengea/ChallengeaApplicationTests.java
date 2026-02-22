4eb26d144e362423579b7cda2cb2472289129bc7
Fernanda Dias <mariadosantodias@gmail.com>
2026-02-22 12:46:07 -0300

    Create ChallengeaApplicationTests.java

---
+++ challengea/src/test/java/com/professionalJava/challengea/ChallengeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.professionalJava.challengea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
