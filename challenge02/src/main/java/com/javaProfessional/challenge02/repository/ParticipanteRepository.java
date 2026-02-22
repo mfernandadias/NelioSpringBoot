6804775701b9f7d833157509b1291acb9bcc7e63
Fernanda Dias <mariadosantodias@gmail.com>
2026-02-22 12:43:39 -0300

    delete repository

--- challenge02/src/main/java/com/javaProfessional/challenge02/repository/ParticipanteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.javaProfessional.challenge02.repository;
-
-import com.javaProfessional.challenge02.entities.Participante;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ParticipanteRepository extends JpaRepository<Participante, Long> {
-}
