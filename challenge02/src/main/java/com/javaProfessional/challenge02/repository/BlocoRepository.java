a34d509b541644129a52dd1fca77983c48404d4e
Fernanda Dias <mariadosantodias@gmail.com>
2026-02-22 12:43:14 -0300

    delete bloco repository

--- challenge02/src/main/java/com/javaProfessional/challenge02/repository/BlocoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.javaProfessional.challenge02.repository;
-
-import com.javaProfessional.challenge02.entities.Bloco;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlocoRepository extends JpaRepository<Bloco, Long> {
-
-}
