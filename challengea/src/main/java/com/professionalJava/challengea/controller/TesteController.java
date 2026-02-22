61928dc9f3b8535c84358e70919d9b0fa0f89471
Fernanda Dias <mariadosantodias@gmail.com>
2026-02-22 12:45:12 -0300

    Create TesteController.java

---
+++ challengea/src/main/java/com/professionalJava/challengea/controller/TesteController.java
@@ -0,0 +1,12 @@
+package com.professionalJava.challengea.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TesteController {
+    @GetMapping("/ola")
+    public String ola() {
+        return "Olá Mundo Spring Boot 🚀";
+    }
+}
