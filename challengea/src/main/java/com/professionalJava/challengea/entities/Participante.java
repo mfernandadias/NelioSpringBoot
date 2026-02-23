6cf782e4b81c2d120171a250a57e75d36ce9b8c9
Fernanda Dias <mariadosantodias@gmail.com>
2026-02-23 10:17:01 -0300

    Update Participante.java

--- challengea/src/main/java/com/professionalJava/challengea/entities/Participante.java
+++ challengea/src/main/java/com/professionalJava/challengea/entities/Participante.java
@@ -7,20 +7,25 @@ import java.util.Set;
 
 
 @Entity
+@Table(name = "tb_participante")
 public class Participante {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+   private Long id;
+   private String name;
+   private String email;
 
-    @ManyToMany(mappedBy = "participantes")
+    @ManyToMany
+    @JoinTable(
+            name = "tb_participante_atividade",
+            joinColumns = @JoinColumn(name = "participante_id"),
+            inverseJoinColumns = @JoinColumn(name = "atividade_id")
+    )
     private Set<Atividade> atividades = new HashSet<>();
 
     public Participante() {
     }
-
-    private String name;
-    private String email;
 
     public Participante(Long id, String email, String name) {
         this.id = id;
@@ -28,4 +33,27 @@ public class Participante {
         this.name = name;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
