50b996896f369407230d12044ba2f70138eb44ad
Fernanda Dias <mariadosantodias@gmail.com>
2026-02-23 10:16:55 -0300

    Update Bloco.java

--- challengea/src/main/java/com/professionalJava/challengea/entities/Bloco.java
+++ challengea/src/main/java/com/professionalJava/challengea/entities/Bloco.java
@@ -5,12 +5,11 @@ import jakarta.persistence.*;
 import java.time.Instant;
 
 @Entity
+@Table(name = "tb_bloco")
 public class Bloco {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-
     private Instant inicio;
     private Instant fim;
 
@@ -18,11 +17,36 @@ public class Bloco {
     @JoinColumn(name = "atividade_id")
     private Atividade atividade;
 
-
-    public Bloco(Long id, Instant inicio, Instant fim, Atividade atividade) {
+    public Bloco(Long id, Instant fim, Instant inicio) {
         this.id = id;
-        this.inicio = inicio;
         this.fim = fim;
-        this.atividade = atividade;
+        this.inicio = inicio;
+    }
+
+    public Bloco() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getFim() {
+        return fim;
+    }
+
+    public void setFim(Instant fim) {
+        this.fim = fim;
+    }
+
+    public Instant getInicio() {
+        return inicio;
+    }
+
+    public void setInicio(Instant inicio) {
+        this.inicio = inicio;
     }
 }
