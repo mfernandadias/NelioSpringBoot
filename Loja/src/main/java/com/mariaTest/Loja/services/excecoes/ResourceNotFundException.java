f6f66d7fd70c8c37b2957980e2988a11b4bed19b
Fernanda Dias <mariadosantodias@gmail.com>
2026-02-24 19:20:33 -0300

    Delete ResourceNotFundException.java

--- Loja/src/main/java/com/mariaTest/Loja/services/excecoes/ResourceNotFundException.java
+++
@@ -1,7 +0,0 @@
-package com.mariaTest.Loja.services.excecoes;
-
-public class ResourceNotFundException extends RuntimeException {
-    public ResourceNotFundException(String msg){
-        super(msg);
-    }
-}
