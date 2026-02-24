ffa4006aca12a65ef9a9548ed7292b2d69bc9ae1
Fernanda Dias <mariadosantodias@gmail.com>
2026-02-24 19:20:22 -0300

    Delete ClientRepository.java

--- Loja/src/main/java/com/mariaTest/Loja/repositories/ClientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mariaTest.Loja.repositories;
-
-import com.mariaTest.Loja.entities.ClientEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientRepository extends JpaRepository<ClientEntity, Long> {
-
-
-}
